73403fbdc7f5931ea1a308acdff60e317498794c
Uos-Math-Ai <ben10sin378302@gmail.com>
2026-04-30 01:13:39 +0900

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],  # ⭐ 여기 수정
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
